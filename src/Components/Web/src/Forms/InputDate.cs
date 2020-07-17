b3ffe7f98403ce8ff993b94f802f75228842e1f7
Ed Charbeneau <ed.charbeneau@gmail.com>
2020-07-17 13:29:23 -0400

    Added options for Date, Time, and Month for InputDate form component.

--- src/Components/Web/src/Forms/InputDate.cs
+++ src/Components/Web/src/Forms/InputDate.cs
@@ -15,18 +15,48 @@ namespace Microsoft.AspNetCore.Components.Forms
     public class InputDate<TValue> : InputBase<TValue>
     {
         private const string DateFormat = "yyyy-MM-dd"; // Compatible with HTML date inputs
+        private const string MonthFormat = "yyyy-MM"; // Compatible with HTML month inputs
+        private const string DateTimeFormat = "yyyy-MM-dd HH:mm:ss"; // Compatible with HTML date time inputs
+        private const string TimeFormat = "HH:mm:ss"; // Compatible with HTML time inputs
+
+        private string HtmlValueFormat => DateInputType switch
+        {
+            DateInputType.Time => TimeFormat,
+            DateInputType.Month => MonthFormat,
+            _ => DateFormat,
+        };
+
+        // Gets the format used propery Parse from HTML value to .NET DateTime.
+        private string DotnetValueFormat => DateInputType == DateInputType.Time ? DateTimeFormat : DateFormat;
+
+        // The HTML input element has multiple formats depending on it's type.
+        // For some types there no direct mapping to the .NET DateTime so padding must be provided with default values.
+        private string MapToDotnetValue(string? value)
+        {
+            return DateInputType switch
+            {
+                DateInputType.Time => $"0001-01-01 {value:TimeFormat}",
+                DateInputType.Month => $"{value:MonthFormat}-01",
+                _ => $"{value:DateTimeFormat}"
+            };
+        }
 
         /// <summary>
         /// Gets or sets the error message used when displaying an a parsing error.
         /// </summary>
         [Parameter] public string ParsingErrorMessage { get; set; } = "The {0} field must be a date.";
 
+        /// <summary>
+        /// Gets or sets the HTML input type. Determines the input fields show to the user.
+        /// </summary>
+        [Parameter] public DateInputType DateInputType { get; set; } = DateInputType.Date;
+
         /// <inheritdoc />
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
             builder.OpenElement(0, "input");
             builder.AddMultipleAttributes(1, AdditionalAttributes);
-            builder.AddAttribute(2, "type", "date");
+            builder.AddAttribute(2, "type", DateInputType.ToString().ToLower());
             builder.AddAttribute(3, "class", CssClass);
             builder.AddAttribute(4, "value", BindConverter.FormatValue(CurrentValueAsString));
             builder.AddAttribute(5, "onchange", EventCallback.Factory.CreateBinder<string?>(this, __value => CurrentValueAsString = __value, CurrentValueAsString));
@@ -39,9 +69,9 @@ namespace Microsoft.AspNetCore.Components.Forms
             switch (value)
             {
                 case DateTime dateTimeValue:
-                    return BindConverter.FormatValue(dateTimeValue, DateFormat, CultureInfo.InvariantCulture);
+                    return BindConverter.FormatValue(dateTimeValue, HtmlValueFormat, CultureInfo.InvariantCulture);
                 case DateTimeOffset dateTimeOffsetValue:
-                    return BindConverter.FormatValue(dateTimeOffsetValue, DateFormat, CultureInfo.InvariantCulture);
+                    return BindConverter.FormatValue(dateTimeOffsetValue, HtmlValueFormat, CultureInfo.InvariantCulture);
                 default:
                     return string.Empty; // Handles null for Nullable<DateTime>, etc.
             }
@@ -57,11 +87,11 @@ namespace Microsoft.AspNetCore.Components.Forms
             bool success;
             if (targetType == typeof(DateTime))
             {
-                success = TryParseDateTime(value, out result);
+                success = TryParseDateTime(MapToDotnetValue(value), out result);
             }
             else if (targetType == typeof(DateTimeOffset))
             {
-                success = TryParseDateTimeOffset(value, out result);
+                success = TryParseDateTimeOffset(MapToDotnetValue(value), out result);
             }
             else
             {
@@ -80,9 +110,11 @@ namespace Microsoft.AspNetCore.Components.Forms
             }
         }
 
-        static bool TryParseDateTime(string? value, [MaybeNullWhen(false)] out TValue result)
+        private bool TryParseDateTime(string? value, [MaybeNullWhen(false)] out TValue result)
         {
-            var success = BindConverter.TryConvertToDateTime(value, CultureInfo.InvariantCulture, DateFormat, out var parsedValue);
+
+
+            var success = BindConverter.TryConvertToDateTime(value, CultureInfo.InvariantCulture, DotnetValueFormat, out var parsedValue);
             if (success)
             {
                 result = (TValue)(object)parsedValue;
@@ -95,9 +127,10 @@ namespace Microsoft.AspNetCore.Components.Forms
             }
         }
 
-        static bool TryParseDateTimeOffset(string? value, [MaybeNullWhen(false)] out TValue result)
+        private bool TryParseDateTimeOffset(string? value, [MaybeNullWhen(false)] out TValue result)
         {
-            var success = BindConverter.TryConvertToDateTimeOffset(value, CultureInfo.InvariantCulture, DateFormat, out var parsedValue);
+
+            var success = BindConverter.TryConvertToDateTimeOffset(value, CultureInfo.InvariantCulture, DotnetValueFormat, out var parsedValue);
             if (success)
             {
                 result = (TValue)(object)parsedValue;
